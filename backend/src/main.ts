0d95e2633c029b9261dc800c421d366d10d16bd3
jonas <josuecondori2002@gmail.com>
2025-07-21 21:35:52 -0500

    feat: configuracion del proyecto

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://frontend:3000',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
